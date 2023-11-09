53168cd700062c927a55b7ec7aeb06b9b872244b
Coder9611 <137375005+Coder9611@users.noreply.github.com>
2023-11-09 13:07:55 +0530

    day 6 part 2

--- Ultra-Mario/Assets/SceneLoader.cs
+++ Ultra-Mario/Assets/SceneLoader.cs
@@ -7,4 +7,13 @@ public class SceneLoader : MonoBehaviour
     {
         SceneManager.LoadScene("level");
     }
+
+    public void ExitScene()
+    {
+        // Get the name of the current scene
+        string currentSceneName = SceneManager.GetActiveScene().name;
+
+        // Load the current scene, effectively restarting it
+        SceneManager.LoadScene(currentSceneName);
+    }
 }
