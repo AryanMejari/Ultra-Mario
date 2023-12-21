ce60e56ed25be43bbdab328bc01374608b7e5d74
Coder9611 <137375005+Coder9611@users.noreply.github.com>
2023-12-21 11:48:39 +0530

    Day 8 Part 2

--- Ultra-Mario/Assets/Conrtol.cs
+++ Ultra-Mario/Assets/Conrtol.cs
@@ -5,86 +5,78 @@ using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5.0f;
-    public float jumpForce = 10.0f;
-    public float rotationSpeed = 100.0f;
-    public AudioClip jumpSound;
-    public AudioClip deathSound;
-    public AudioClip coinCollectSound;
+    public float moveSpeed = 5.0f; // Player movement speed
+    public float jumpForce = 10.0f; // Force applied when the player jumps
+    public AudioClip jumpSound; // Sound played when jumping
 
-    private Rigidbody2D rb;
-    private bool isGrounded;
-    private AudioSource audioSource;
-    private int points = 0;
-    public TextMeshProUGUI pointsText;
+    private Rigidbody2D rb; // Reference to the player's Rigidbody component
+    private bool isGrounded; // Flag to check if the player is grounded
+    private AudioSource audioSource; // Reference to the AudioSource component
+    private int points = 0; // Player's points
+    public TextMeshProUGUI pointsText; // Reference to the UI text displaying points
 
-    private bool isGameOver = false;
-    private bool canJump = true;
+    private bool isGameOver = false; // Flag to check if the game is over
+    private bool canJump = true; // Flag to control consecutive jumps
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        audioSource = GetComponent<AudioSource>();
-        UpdatePointsText();
+        rb = GetComponent<Rigidbody2D>(); // Get the Rigidbody component of the player
+        audioSource = GetComponent<AudioSource>(); // Get the AudioSource component of the player
+        UpdatePointsText(); // Update the UI text displaying points
     }
 
     private void Update()
     {
-        if (isGameOver) return;
+        if (isGameOver) return; // If the game is over, stop updating
 
-        HandleTouchInput();
+        HandleTouchInput(); // Handle touch input for mobile controls
 
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        Vector2 movement = new Vector2(moveHorizontal, 0.0f) * moveSpeed;
-        rb.velocity = new Vector2(movement.x, rb.velocity.y);
+        float moveHorizontal = Input.GetAxis("Horizontal"); // Get input for horizontal movement
+        Vector2 movement = new Vector2(moveHorizontal, 0.0f) * moveSpeed; // Calculate movement based on input
+        rb.velocity = new Vector2(movement.x, rb.velocity.y); // Apply movement to the player
 
-        CheckGroundedStatus();
+        CheckGroundedStatus(); // Check if the player is grounded
 
+        // Handle jumping when the player is grounded and the jump button is pressed
         if (isGrounded && Input.GetButtonDown("Jump") && canJump)
         {
-            Jump();
-            canJump = false;
-            StartCoroutine(EnableJumpAfterDelay(1.3f));
+            Jump(); // Perform a jump
+            canJump = false; // Prevent consecutive jumps
+            StartCoroutine(EnableJumpAfterDelay(1.3f)); // Enable jump after a delay
         }
-
-        RotatePlayer(moveHorizontal);
     }
 
     private void HandleTouchInput()
     {
-        if (Input.touchCount > 0)
+        if (Input.touchCount > 0) // Check if there is any touch input
         {
-            Touch touch = Input.GetTouch(0);
+            Touch touch = Input.GetTouch(0); // Get the first touch
 
-            // Define screen areas for actions: upper half for jumping, lower half for left/right movement
             float screenHeight = Screen.height;
             float screenWidth = Screen.width;
-            float upperHalf = screenHeight * 0.5f;
+            float upperHalf = screenHeight * 0.75f; // Upper portion for jumping
+            float leftThird = screenWidth * 0.25f; // Left third for moving left
 
-            if (touch.phase == TouchPhase.Began)
+            if (touch.phase == TouchPhase.Began) // Check if the touch has just started
             {
-                Vector2 touchPos = touch.position;
+                Vector2 touchPos = touch.position; // Get the touch position
 
-                // Jump action: if touch is in the upper half of the screen
+                // Jump action: if touch is in the upper portion of the screen
                 if (touchPos.y > upperHalf && canJump)
                 {
-                    Jump();
-                    canJump = false;
-                    StartCoroutine(EnableJumpAfterDelay(1.3f));
+                    Jump(); // Perform a jump
+                    canJump = false; // Prevent consecutive jumps
+                    StartCoroutine(EnableJumpAfterDelay(1.3f)); // Enable jump after a delay
                 }
-                // Movement action: if touch is in the lower half of the screen
-                else if (touchPos.y <= upperHalf)
+                // Left movement: if touch is in the left third of the screen
+                else if (touchPos.x < leftThird)
                 {
-                    // Left movement: if touch is on the left side of the lower half
-                    if (touchPos.x < screenWidth * 0.5f)
-                    {
-                        MoveCharacter(-1);
-                    }
-                    // Right movement: if touch is on the right side of the lower half
-                    else
-                    {
-                        MoveCharacter(1);
-                    }
+                    MoveCharacter(-1); // Move the player left
+                }
+                // Right movement: if touch is in the right portion of the screen
+                else if (touchPos.x > leftThird * 2)
+                {
+                    MoveCharacter(1); // Move the player right
                 }
             }
         }
@@ -92,40 +84,51 @@ public class PlayerMovement : MonoBehaviour
 
     private void MoveCharacter(float direction)
     {
-        Vector2 movement = new Vector2(direction * moveSpeed, rb.velocity.y);
-        rb.velocity = movement;
-    }
-
-    private void RotatePlayer(float moveHorizontal)
-    {
-        float rotationAmount = -moveHorizontal * rotationSpeed * Time.deltaTime;
-        transform.Rotate(Vector3.forward, rotationAmount);
+        Vector2 movement = new Vector2(direction * moveSpeed, rb.velocity.y); // Calculate movement
+        rb.velocity = movement; // Apply movement to the player
     }
 
     private IEnumerator EnableJumpAfterDelay(float delay)
     {
-        yield return new WaitForSeconds(delay);
-        canJump = true;
+        yield return new WaitForSeconds(delay); // Wait for the specified delay
+        canJump = true; // Enable jumping after the delay
     }
 
     private void CheckGroundedStatus()
     {
-        isGrounded = transform.position.y <= 0.1f;
+        isGrounded = transform.position.y <= 0.1f; // Check if the player is close to the ground
     }
 
     private void Jump()
     {
-        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        PlayJumpSound();
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse); // Apply upward force for jumping
+        PlayJumpSound(); // Play the jump sound
     }
 
     private void PlayJumpSound()
     {
         if (jumpSound != null)
         {
-            audioSource.PlayOneShot(jumpSound);
+            audioSource.PlayOneShot(jumpSound); // Play the jump sound
         }
     }
 
-    // Other methods for collisions, audio, points, etc.
+    // Other methods for collision handling, audio, points, etc.
+
+    private void UpdatePointsText()
+    {
+        if (pointsText != null)
+        {
+            pointsText.text = "Points: " + points.ToString(); // Update the UI text with points
+        }
+    }
+
+    private IEnumerator RestartLevelAfterDelay(float delay)
+    {
+        isGameOver = true; // Set game over flag
+
+        yield return new WaitForSeconds(delay); // Wait for the specified delay
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Restart the level
+    }
 }
